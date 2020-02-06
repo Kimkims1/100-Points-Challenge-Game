e781baf527d11249c9ac90eb7bb20c78f2d67b7a
Kim kim <kimanthikevin76@gmail.com>
2020-02-06 21:42:04 +0300

    100 Points Game Challenge

---
+++ app.js
@@ -0,0 +1,46 @@
+//use arrays keep code clean
+let scores, roundScore, activePlayer, dice;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+//document.querySelector("#current-" + activePlayer).textContent = dice;
+document.querySelector(".dice").style.display = "none";
+
+document.querySelector("#score-0").textContent = "0";
+document.querySelector("#score-1").textContent = "0";
+document.querySelector("#current-0").textContent = "0";
+document.querySelector("#current-1").textContent = "0";
+
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  //Do sth here
+  let dice = Math.floor(Math.random() * 6) + 1;
+
+  //Display the result
+  let diceDom = document.querySelector(".dice");
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + dice + ".png";
+
+  //Update roundScore
+  if (dice !== 1) {
+    //Add score
+    roundScore += dice; //roundScore = roundScore + dice
+    document.querySelector("#current-" + activePlayer).textContent = roundScore;
+  } else {
+    //Next Player
+    //Using ternary Operator
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    //if (activePlayer === 0) { Same thing as ternary Operator
+    // activePlayer = 1;
+    // } else {
+    //  activePlayer = 0;
+    // }
+    roundScore = 0;
+    document.getElementById("current-0").textContent = "0";
+    document.getElementById("current-1").textContent = "0";
+
+    document.querySelector(".player-0-panel").classList.remove("active");
+    document.querySelector(".player-1-panel").classList.add("active");
+  }
+});
